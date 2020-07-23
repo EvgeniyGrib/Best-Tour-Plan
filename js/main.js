95db760b79e04c72c7f2d8a2e6ced53b46560173
EvgeniyGrib <evg.grib@mail.ru>
2020-07-23 23:21:17 +0300

    fix: background-color map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
-
   // Navigation arrows
   navigation: {
     nextEl: ".hotel-slider__button--next",
@@ -17,7 +16,6 @@ var hotelSlider = new Swiper(".hotel-slider", {
 var reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
-
   // Navigation arrows
   navigation: {
     nextEl: ".reviews-slider__button--next",
@@ -28,3 +26,24 @@ var reviewsSlider = new Swiper(".reviews-slider", {
     onlyInViewport: false,
   },
 });
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [12.934923151175724, 100.88307986706273],
+    zoom: 15,
+    // behaviors: [], // если при наведении крутить колесик, то карта не будет зумиться
+  });
+
+  myMap.geoObjects;
+
+  // var placemark = new ymaps.Placemark(
+  //   [12.934923151175724, 100.88307986706273],
+  //   {}
+  // );
+
+  // map.geoObjects.add(placemark);
+}
