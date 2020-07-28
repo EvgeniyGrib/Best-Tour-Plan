3affa2006df2b7f3017f4ff289df507a0b30d823
EvgeniyGrib <evg.grib@mail.ru>
2020-07-28 09:41:40 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -53,3 +53,13 @@ function init() {
 // ======== PARALAX ===================
 
 $(".newsletter-parallax").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
+
+// ============= mobile menu =================
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
