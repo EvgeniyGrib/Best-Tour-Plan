59fd59024a37eb71820e1635881198aa076e23ec
EvgeniyGrib <evg.grib@mail.ru>
2020-07-31 14:59:54 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,65 +1,93 @@
-// ======== SWIPER ==============
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  effect: "coverflow",
-});
-
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-// ========= MAP ==================
-
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [12.934923151175724, 100.88307986706273],
-    zoom: 15,
-    // behaviors: [], // если при наведении крутить колесик, то карта не будет зумиться
+$(document).ready(function () {
+  // ======== SWIPER ==============
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    effect: "coverflow",
   });
-  // Метка ------------------
-  var placemark = new ymaps.Placemark(
-    [12.934923151175724, 100.88307986706273],
-    {
-      hintContent: "Hilton Hotel", // при наведении всплывает подсказка
+
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  // ========= MAP ==================
+
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [12.934923151175724, 100.88307986706273],
+      zoom: 15,
+      // behaviors: [], // если при наведении крутить колесик, то карта не будет зумиться
+    });
+    // Метка ------------------
+    var placemark = new ymaps.Placemark(
+      [12.934923151175724, 100.88307986706273],
+      {
+        hintContent: "Hilton Hotel", // при наведении всплывает подсказка
+      }
+    );
+    myMap.geoObjects.add(placemark);
+  }
+
+  // ============= mobile menu =================
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom_visible");
+  });
+
+  // ================== MODAL ==========================
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal() {
+    event.preventDefault(); // чтобы не перебрасывало наверх страницы. нужно указывать ивент в ценкции и тут
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+  window.onkeydown = () => {
+    if (event.key == "Escape") {
+      closeModal();
     }
-  );
-  myMap.geoObjects.add(placemark);
-}
+  };
+});
 
 // ======== PARALAX ===================
 
 $(".newsletter-parallax").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
-
-// ============= mobile menu =================
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom_visible");
-});
---
+++ send.php
@@ -0,0 +1,70 @@
+<?php 
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон  :</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+$sendMail = "
+<b>Почта клиента для рассылки новостей: $email</b>
+";
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'gribevgenij779@gmail.com'; // Логин на почте
+    $mail->Password   = 'XW6ucafLRepMaHZ'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('gribevgenij779@gmail.com', 'Евгений Гриб'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    // $mail->addAddress('youremail@yandex.ru');  
+    $mail->addAddress('evg.grib@mail.ru'); // Ещё один, если нужен
+
+// Отправка сообщения
+if ($email == null) {
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+} else {
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $sendMail;
+}
+    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
